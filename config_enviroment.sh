bcacbfecdc53906772503ce7157898ef644a5c53
zxhero <zhangxu415@mails.ucas.edu.cn>
2019-03-06 17:02:35 +0800

    Hardware function test program

---
+++ config_enviroment.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+export RISCV=/home/zxhero/HCPF-pro/riscv-tools-build
+export PATH=$PATH:/home/zxhero/HCPF-pro/riscv-tools-build/bin/
+
+# rm $RISCV/bin/riscv64-unknown-elf-pk
+# ln -s $RISCV/riscv64-unknown-elf/bin/pk $RISCV/bin/riscv64-unknown-elf-pk 
+
+echo $RISCV
+echo $PATH
---
+++ config_network.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+while :
+do
+      sudo ifconfig enp3s0 192.168.1.5
+done
